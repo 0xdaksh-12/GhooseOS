06cd20f2e823a204bf5137e51de18e6ef3850969
0xdaksh-12 <0xdaksh.12@gmail.com>
2026-07-10 18:58:03 +0530

    feat: print Hello World using BIOS interrupts

--- src/main.asm
+++ src/main.asm
@@ -1,13 +1,57 @@
 org 0x7C00
 bits 16
 
+%define ENDL 0x0D, 0x0A
+
+start:
+    jmp main
+
+;
+; Prints a string to the screen
+; Params:
+;   - ds:si points to string
+;
+
+puts:
+    push si
+    push ax
+
+.loop:
+    lodsb               ; load next character into al
+    or al, al           ; verify if the next character is null??
+    jz .done
+
+    mov ah, 0x0E        ; call bios interrupt
+    int 0x10
+
+    jmp .loop           ; Continue printing
+
+.done:
+    pop ax
+    pop si
+    ret
 
 main:
-  hlt
+
+    ; Initialize segments
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    ; Initialize stack
+    mov ss, ax
+    mov sp, 0x7C00
+
+    ; Print message
+    mov si, msg_hello
+    call puts
 
 .halt:
-  jmp .halt
+    hlt
+    jmp .halt
 
+msg_hello:
+    db 'Hello World!', ENDL, 0
 
 times 510-($-$$) db 0
-dw 0AA55h
+dw 0xAA55
